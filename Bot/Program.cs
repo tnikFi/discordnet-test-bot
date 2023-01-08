dfa14f03b7a62d9ee1343f8863902cdf9c9d6962
tnikFi <95296580+tnikFi@users.noreply.github.com>
2023-01-08 16:39:47 +0200

    Bot can now log in

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+
+public class Program {
+    private DiscordSocketClient _client;
+    
+    public static Task Main(String[] args) => new Program().MainAsync();
+
+    public async Task MainAsync() {
+        _client = new DiscordSocketClient();
+        
+        // Token should be stored in an environment variable called "token".
+        var token = Environment.GetEnvironmentVariable("token");
+        
+        // Log in to Discord
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        // Block this task until the program is closed
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg) {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
